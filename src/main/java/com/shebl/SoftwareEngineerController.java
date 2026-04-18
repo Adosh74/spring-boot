65e6e6a2cf7f5890767319325f34fa2a5aff3ae4
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-04-18 23:20:21 +0200

    Add PostgreSQL configuration and implement SoftwareEngineer API

--- src/main/java/com/shebl/SoftwareEngineerController.java
+++ src/main/java/com/shebl/SoftwareEngineerController.java
@@ -1,4 +1,20 @@
 package com.shebl;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers () {
+        return List.of(
+                new SoftwareEngineer(1, "Mohamed Shebl", "js, node"),
+                new SoftwareEngineer(2, "Mohamed Nashaat", "js, node")
+        );
+    }
 }
