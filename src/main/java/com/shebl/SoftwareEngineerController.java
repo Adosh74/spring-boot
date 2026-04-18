9e8992f68f37050eaf8dcee5e33a833b4c5890c6
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-04-18 23:42:28 +0200

    Implement SoftwareEngineer entity, repository, service, and update controller to fetch data from the database

--- src/main/java/com/shebl/SoftwareEngineerController.java
+++ src/main/java/com/shebl/SoftwareEngineerController.java
@@ -10,11 +10,13 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController (SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
     @GetMapping
     public List<SoftwareEngineer> getEngineers () {
-        return List.of(
-                new SoftwareEngineer(1, "Mohamed Shebl", "js, node"),
-                new SoftwareEngineer(2, "Mohamed Nashaat", "js, node")
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
     }
 }
---
+++ src/main/java/com/shebl/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.shebl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
